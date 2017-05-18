008c54eba18f22302015acdbf9559ba2099215fa
vanJoseph <donovan.wilder@gmail.com>
2017-05-18 04:20:44 -0400

    Nearly completed the result activity

---
+++ app/src/main/java/com/example/mikaila/bingewatcher/ResultActivity.java
@@ -0,0 +1,10 @@
+package com.example.mikaila.bingewatcher;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dwilder1181 on 5/18/2017.
+ */
+
+public class ResultActivity extends AppCompatActivity {
+}
